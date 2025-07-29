b402f77ae412f3e5b3b9cbfe7aaa1afba4142803
igorgp06 <igorgoncalvesp20@gmail.com>
2025-07-28 23:49:18 -0300

    [DEPLOY 1.0] Deploy inicial do projeto com os arquivos base

---
+++ src/main/java/br/com/igorgpDEV/backend_Junior_Itau/BackendJuniorItauApplication.java
@@ -0,0 +1,13 @@
+package br.com.igorgpDEV.backend_Junior_Itau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJuniorItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJuniorItauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/igorgpDEV/backend_Junior_Itau/BackendJuniorItauApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igorgpDEV.backend_Junior_Itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJuniorItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
