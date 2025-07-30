3efe912e94d20c478ea4278c1553f84890718817
igorgp06 <igorgoncalvesp20@gmail.com>
2025-07-29 23:43:16 -0300

    [WORK 1.4] Criação da lógica inicial das estatisticas

--- src/main/java/br/com/igorgpDEV/backend_Junior_Itau/transacao/TransacaoRepository.java
+++ src/main/java/br/com/igorgpDEV/backend_Junior_Itau/transacao/TransacaoRepository.java
@@ -2,8 +2,12 @@ package br.com.igorgpDEV.backend_Junior_Itau.transacao;
 
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.DoubleStream;
 
 @Repository
 public class TransacaoRepository {
@@ -16,5 +20,15 @@ public class TransacaoRepository {
 
     public void limpar() {
         transacoes.clear();
+    }
+
+    public Object estatistica(OffsetDateTime horaInicial) {
+
+        final BigDecimal[] valoresFiltrados = transacoes.stream()
+                .filter(t -> t.getDataHora().isAfter(horaInicial) || t.getDataHora().equals(horaInicial))
+                .map(t -> t.getValor()).toArray(BigDecimal[]::new);
+
+        DoubleStream doubleStream = Arrays.stream(valoresFiltrados).mapToDouble(BigDecimal::doubleValue);
+        return doubleStream.summaryStatistics();
     }
 }
